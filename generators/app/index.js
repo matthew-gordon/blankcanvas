de0189898984b70dc69e3a1f02a322a7af534054
Matthew Gordon <laxredemption5525@gmail.com>
2017-06-04 15:34:16 -0600

    Use data

--- generators/app/index.js
+++ generators/app/index.js
@@ -82,6 +82,7 @@ module.exports = class extends Generator {
         if (err) {
           throw err;
         }
+        console.log(data);
         this.fs.copy(
           this.templatePath('package.json'),
           this.destinationPath('package.json')
